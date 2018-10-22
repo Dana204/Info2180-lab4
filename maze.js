13c0f5e41d0a19238d41eb1c849ee4d68c50affd
Dana Campbell <danacam28@gmail.com>
2018-10-21 20:01:21 -0500

    got all borders to change to red on hover

--- maze.js
+++ maze.js
@@ -1,3 +1,10 @@
 function changeToRed() {
     document.getElementById("boundary1").style.borderColor = "red";
+
+
+var boundaries = maze.querySelectorAll("div.boundary");
+
+boundaries.forEach(function(item) {
+    item.style.borderColor= "red";
+});
 }

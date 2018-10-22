3512352b555a132ac739c1db1ee750fff88bca8c
Dana Campbell <danacam28@gmail.com>
2018-10-21 21:39:37 -0500

    added changeToNormal()

--- maze.js
+++ maze.js
@@ -6,3 +6,21 @@ function changeToRed() {
         item.style.borderColor= "red";
     });
 }
+
+function changeToNormal() {
+    //single border: document.getElementById("boundary1").style.borderColor = "red";
+    var boundaries = maze.querySelectorAll("div.boundary");
+
+    boundaries.forEach(function(item) {
+        item.style.borderColor= "black";
+    });
+}
+
+function win(){
+    var popup = document.getElementById('#end'); 
+    e.onmouseover = function() {
+        alert('YOU WIN!');
+     }
+   
+}
+
